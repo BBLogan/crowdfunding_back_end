e28fe2f738a6d8f89f4ebdf2687fae482ec86919
BexLogan <bek.jayne@gmail.com>
2024-01-11 20:43:09 +0800

    finishing the initial project set up (prior to handling model relationships)

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Projects
 from .serializers import ProjectSeralizer
+from django.http import Http404
+from rest_framework import status
 
 class ProjectList(APIView):
 
@@ -9,4 +11,28 @@ class ProjectList(APIView):
         projects = Projects.objects.all()
         serializer = ProjectSeralizer(projects, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ProjectSeralizer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+                )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
+class ProjectDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Projects.objects.get(pk=pk)
+        except Projects.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSeralizer(project)
+        return Response(serializer.data)
