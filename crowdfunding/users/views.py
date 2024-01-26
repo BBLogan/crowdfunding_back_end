58de57196446c196f2f9b20e1240ba98810ab4bc
BexLogan <bek.jayne@gmail.com>
2024-01-26 21:10:36 +0800

    authentication nonsense & paritial install of django toolbar

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -14,7 +14,7 @@ class CustomUserList(APIView):
     def post(self, request):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(owner=request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
